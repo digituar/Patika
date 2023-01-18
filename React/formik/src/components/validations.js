50b5f2d012b4338d20198a968759e44150b1b682
digituar <34481389+digituar@users.noreply.github.com>
2023-01-19 00:18:03 +0100

    Delete React/formik directory

--- React/formik/src/components/validations.js
+++
@@ -1,9 +0,0 @@
-import { object, string, number, date, InferType, ref } from 'yup';
-
-const validations = object().shape({
-    email: string().email("Geçerli bir email girin!").required("Zorunlu alan"),
-    password: string().min(5, 'Parolanız en az beş karakter olalıdır').required("Zorunlu alan"),
-    passwordConfim: string().oneOf([ref("password")], 'Parolalar uyuşmuyor').required("Zorunlu alan")
-});
-
-export default validations;
